9ca4205d8532cc83539e5db92a08dfb772c29e37
EliasPauloProgramador <eliaspauloeliezer@gmail.com>
2026-06-04 07:52:05 +0100

    feat: páginas em construção com componente profissional

--- app/tecnico/servicos/page.tsx
+++ app/tecnico/servicos/page.tsx
@@ -1,1 +1,2 @@
-export default function Page() { return <div>Em construção</div> }
+import ComingSoon from '@/components/ui/ComingSoon';
+export default function Page() { return <ComingSoon pageName="Gestão de Serviços" backLink="/tecnico/dashboard" />; }
