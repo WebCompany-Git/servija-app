d83bfc12e696d7659769e469649bf7471f30c693
WebCompany-Git <webcompanyon@gmail.com>
2026-06-05 00:38:48 +0100

    hey

--- app/tecnico/[id]/page.tsx
+++ app/tecnico/[id]/page.tsx
@@ -1,6 +1,6 @@
-<<<<<<< HEAD
+
 import PerfilPublicoTecnicoClient from './PerfilPublicoTecnicoClient'
-=======
+
 export const dynamic = "force-dynamic";
 
 'use client'
@@ -10,7 +10,6 @@ import { createClient } from '@/lib/supabase/client'
 import SeloBadge from '@/components/selos/SeloBadge'
 import Avatar from '@/components/ui/Avatar'
 import LoadingSpinner from '@/components/ui/LoadingSpinner'
->>>>>>> c6580802b2687abcbbdc6d1078b6e979e1041bb2
 
 export const dynamic = 'force-dynamic'
 
